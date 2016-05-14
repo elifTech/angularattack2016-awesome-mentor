cf39a1a380b38838d5dea8c1d0b5acbe9bd2d005
esvit <esvit666@gmail.com>
2016-05-14 16:54:11 +0300

    add awesome service

--- app/mentor/controllers/profession-content.controller.ts
+++ app/mentor/controllers/profession-content.controller.ts
@@ -4,8 +4,9 @@ import {ROUTER_DIRECTIVES, Router, RouteConfig} from '@angular/router-deprecated
 import {Select, SELECT_DIRECTIVES} from 'ng2-select';
 
 import {CourseraService} from '../../services/coursera.service';
-
 import {YouTubeService} from '../../services/youtube.service';
+import {AwesomeService} from '../../services/awesome.service';
+
 import {FORM_PROVIDERS, FormBuilder, Validators} from '@angular/common';
 import { Observable } from 'rxjs/Observable';
 
@@ -20,7 +21,8 @@ import { Observable } from 'rxjs/Observable';
     providers: [
         FORM_PROVIDERS,
         CourseraService,
-        YouTubeService
+        YouTubeService,
+        AwesomeService
     ]
 })
 export class MentorProfessionContentController {
@@ -30,10 +32,14 @@ export class MentorProfessionContentController {
     professionForm: any;
     youTubeResults$: Observable<any>;
     courseraResults$: Observable<any>;
+    awesomeResults$: Observable<any>;
 
 
-    constructor(protected router:Router, private _courseraService: CourseraService,
-                private _youTubeService: YouTubeService, private _formBuilder: FormBuilder) {
+    constructor(protected router:Router,
+                private _courseraService: CourseraService,
+                private _youTubeService: YouTubeService,
+                private _awesomeService: AwesomeService,
+                private _formBuilder: FormBuilder) {
         console.log('MentorProfessionContentController');
 
         this.professionForm = this._formBuilder.group({
@@ -49,6 +55,12 @@ export class MentorProfessionContentController {
             .debounceTime(400)
             .distinctUntilChanged()
             .switchMap(term => this._courseraService.search(term));
+
+        this.awesomeResults$ = this.professionForm.controls.queryStringInput.valueChanges
+            .debounceTime(400)
+            .distinctUntilChanged()
+            .switchMap(term => this._awesomeService.search(term));
+
     }
     
     public saveItem()
