3d8761ff1143c4639cf11cd2fb9a99dee462b666
marina <abisfadone@gmail.com>
2016-05-14 22:55:30 +0300

    add rename/remove level

--- app/models/level.model.ts
+++ app/models/level.model.ts
@@ -2,13 +2,17 @@ import {LevelItem} from "../models/level-item.model";
 
 export class Level {
     public name: string;
+    public oldName: string;
+    public isDeleted: boolean = false;
+    public isRenamed: boolean = false;
+    public isNew: boolean = false;
     public items: LevelItem[] = [];
 
     constructor(raw: any) {
         if(typeof(raw) == "string") {
             this._parse(raw);
         } else {
-            this.name = raw.name;
+            this.oldName = this.name = raw.name;
         }
         //console.log('raw', raw);
     }
@@ -20,7 +24,7 @@ export class Level {
 
     public toMd()
     {
-        var markdown = '# ' + this.name + '\n\n';
+        var markdown = '# ' + this.name + "\n\n";
         this.items.forEach((item:LevelItem) => {
             markdown += item.toMd();
         })
