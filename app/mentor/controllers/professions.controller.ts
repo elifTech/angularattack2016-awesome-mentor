44e583a7086d8403f3031747dbe8f2e8ff462c54
Polluxx <yuriy.kosakivsky@gmail.com>
2016-05-14 08:41:02 +0300

    add document model

--- app/mentor/controllers/professions.controller.ts
+++ app/mentor/controllers/professions.controller.ts
@@ -2,16 +2,19 @@ import {
     Component
 } from '@angular/core';
 import {CORE_DIRECTIVES} from '@angular/common';
+import {Document} from '../models/document.model';
 @Component({
     selector: 'as-professions',
     templateUrl: '/views/mentor/professions.html',
     directives: [CORE_DIRECTIVES]
 })
 export class ProfessionsController {
-    // public doc: Document;
+    public doc: Document;
+    public list: Document[];
     
     constructor(){
-        // this.doc = new Document();
+        this.doc = new Document('default');
+        this.list = [this.doc];
     }
     
 }
---
+++ app/mentor/models/document.model.ts
@@ -0,0 +1,22 @@
+export class Document {
+    public title: string;
+    public type: string;
+    public rating: number;
+    public source: string;
+    public done: boolean;
+
+    constructor(title: string) {
+        this.title = title;
+        // this.type = type;
+        // this.source = source;
+    }
+
+    rate(rating: number) {
+        this.rating = rating;
+    }
+
+    close(isDone: boolean, rating: number) {
+        this.rate(rating || 0);
+        this.done = isDone;
+    }
+}
