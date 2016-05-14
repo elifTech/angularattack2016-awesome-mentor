e1ce705b195bb0f1665fcd2d831ce174d6c32f10
Polluxx <yuriy.kosakivsky@gmail.com>
2016-05-14 09:16:59 +0300

    fix controller and view

--- app/mentor/controllers/professions.controller.ts
+++ app/mentor/controllers/professions.controller.ts
@@ -2,18 +2,26 @@ import {
     Component
 } from '@angular/core';
 import {CORE_DIRECTIVES} from '@angular/common';
+import {ROUTER_DIRECTIVES, ROUTER_PROVIDERS, RouteConfig} from '@angular/router-deprecated';
 import {Profession} from '../../models/profession.model';
 @Component({
     templateUrl: '/views/mentor/professions.html',
-    directives: [CORE_DIRECTIVES]
+    directives: [
+        CORE_DIRECTIVES,
+        ROUTER_DIRECTIVES
+    ],
+    providers: [
+        ROUTER_PROVIDERS
+    ]
 })
 export class MentorProfessionsController {
     public doc: Profession;
-    public list: Profession[];
-    
+    public docs: Profession[];
+
     constructor(){
         this.doc = new Profession('default');
-        this.list = [this.doc];
+        let doc2 = new Profession('second');
+        this.docs = [this.doc, doc2];
     }
     
 }
