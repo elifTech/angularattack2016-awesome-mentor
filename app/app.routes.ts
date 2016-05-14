e0b1233bc86b00abb696fa0b672bae6e96d6a54e
marina <abisfadone@gmail.com>
2016-05-14 10:47:17 +0300

    add profession content

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,6 +2,7 @@ import {RouteDefinition} from '@angular/router-deprecated';
 import {HomeComponent} from './home/home.component';
 import {MentorProfessionsController} from "./mentor/controllers/professions.controller";
 import {MentorProfessionEditController} from './mentor/controllers/profession-edit.controller';
+import {MentorProfessionContentController} from './mentor/controllers/profession-content.controller';
 import {MentorLoginController} from "./mentor/controllers/login.controller";
 
 export var APP_ROUTES: RouteDefinition[] = [
@@ -9,5 +10,6 @@ export var APP_ROUTES: RouteDefinition[] = [
     { path: '/mentor', name: 'MentorLogin', component: MentorLoginController },
     { path: '/mentor/professions', name: 'MentorProfessions', component: MentorProfessionsController },
     { path: '/mentor/profession/:id/edit', name: 'MentorProfessionEdit', component: MentorProfessionEditController },
+    { path: '/mentor/profession/:id/content', name: 'MentorProfessionContent', component: MentorProfessionContentController },
     { path: '/mentor/profession/create', name: 'MentorProfessionCreate', component: MentorProfessionEditController },
 ];
