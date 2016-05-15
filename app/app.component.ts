2d84151860a4638c42a45e5f6e52b06b70fd9594
esvit <esvit666@gmail.com>
2016-05-15 20:48:13 +0300

    fix tether service

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,6 +7,7 @@ import {UserModel} from "./models/user.model";
 import {Auth} from 'ng2-ui-auth';
 import {GithubService} from "./services/github.service";
 import {ToastrService} from "./services/toastr.service";
+import {TetherService} from "./services/tether.service";
 
 @Component({
     selector: 'as-main-app',
@@ -22,7 +23,7 @@ export class AppComponent {
 
     private auth: Auth;
 
-    constructor(private authService:AuthService, public router: Router, private toastr: ToastrService) {
+    constructor(private authService:AuthService, public router: Router, private toastr: ToastrService, private tether: TetherService) {
         authService.init();
 
         this.auth = AuthService.auth;
