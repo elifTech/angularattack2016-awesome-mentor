cd27b5a61d0fab92658da9bd072d46ac4f192fcc
Polluxx <yuriy.kosakivsky@gmail.com>
2016-05-15 19:50:15 +0300

    add fix for degree

---
+++ app/models/document.model.ts
@@ -0,0 +1,9 @@
+import {PublicLevelItem} from "./public-level-item.model";
+
+export class DocumentModel {
+    public type: string;
+    public id: string;
+    public courses: PublicLevelItem[];
+    public name: string;
+    public modified: string;
+}
--- app/services/google.service.ts
+++ app/services/google.service.ts
@@ -3,6 +3,8 @@ import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Auth} from 'ng2-ui-auth';
 
+import {Base64Service} from '../services/base64.service';
+
 @Injectable()
 export class GoogleService {
     private _token:string;
@@ -42,6 +44,127 @@ export class GoogleService {
                 user.avatar_url = user.cover.coverPhoto.url;
             }
             next(user);
+        });
+    }
+    
+    driveAuth(): Promise<any> {
+        if (this.auth.isAuthenticated()) {
+            this._token = this.auth.getToken();
+        }
+
+        if(!this._token) {
+            gapi.auth.init(function ():any {
+
+            });
+            gapi.auth.signIn(this._token);
+        }
+        /* jshint camelCase: false */
+        var token: any = gapi.auth.getToken();
+
+        var now = Date.now() / 1000;
+        if (token /*&& ((token.expires_at - now) > (60))*/) {
+
+            return new Promise((resolve, reject) => {resolve(token)});
+        } else {
+            var params = {
+                'client_id': '616075536950-pauau0e7u0c980llqh99ftvg3sd32c61.apps.googleusercontent.com',
+                'scope': [
+                    'https://www.googleapis.com/auth/drive.file',
+                    'https://www.googleapis.com/auth/drive.install'
+                ],
+                'immediate': false,
+                //'user_id': userId
+            };
+
+            return new Promise((resolve, reject) => {
+                gapi.auth.authorize(params, function (result) {
+                    if (result && !result.error) {
+                        resolve(result);
+                    } else {
+                        reject(result);
+                    }
+                });
+            });
+        }
+    }
+
+    public createDocument = function (title, body) {
+        console.log(title, body);
+        return new Promise((resolve, reject) => {
+            var onComplete = function (result) {
+                if (result && !result.error) {
+                    resolve(result);
+                } else {
+                    reject(result);
+                }
+            };
+            gapi.client.request({
+                'path': '/upload/drive/v2/files',
+                'method': 'POST',
+                'body': JSON.stringify({
+                    title: title,
+                    body: body,
+                    mimeType: 'application/json'
+                })
+            }).execute(onComplete);
+        });
+    }
+
+    public updateDocument = function(id, title, body) {
+        return new Promise((resolve, reject) => {
+            var onComplete = function (result) {
+                if (result && !result.error) {
+                    resolve(result);
+                } else {
+                    reject(result);
+                }
+            };
+            gapi.client.request({
+                'path': '/upload/drive/v2/files/'+id,
+                'method': 'PUT',
+                //'params': {'uploadType': 'multipart', 'alt': 'json'},
+                'body': JSON.stringify({
+                    title: title,
+                    description: 'AwesomeMentor configuration',
+                    body: body,
+                    mimeType: 'application/json'
+                })
+            }).execute(onComplete);
+        });
+    }
+
+    public getDocument = function(id) {
+        return new Promise((resolve, reject) => {
+            var onComplete = function (result) {
+                if (result && !result.error) {
+                    resolve(result);
+                } else {
+                    reject(result);
+                }
+            };
+            gapi.client.request({
+                'path': '/upload/drive/v2/files/'+id,
+                'method': 'GET'
+            }).execute(onComplete);
+        });
+    }
+
+    public findDocument = function(name) {
+        return new Promise((resolve, reject) => {
+            var onComplete = function (result) {
+                if (result && !result.error) {
+                    resolve(result.items[0]);
+                } else {
+                    reject(result);
+                }
+            };
+            gapi.client.request({
+                'path': '/drive/v2/files/',
+                'method': 'GET',
+                'params': {
+                    'q': "title = '" + name + "'"
+                }
+            }).execute(onComplete);
         });
     }
 }
